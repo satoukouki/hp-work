9d1499398f4132e852c689e503c546ec60886bf9
Yoshiki Aoki <aoki@example.com>
2023-06-29 14:55:03 +0900

    About実装

--- src/modules/home/About.tsx
+++ src/modules/home/About.tsx
@@ -1,9 +1,12 @@
 import { Box, SxProps, Theme } from "@mui/material";
-
+import Aboutimg from "../../resources/work1/home/about_illustration.png";
 export const About = () => {
   return (
     <Box className="About" sx={sx}>
-      <Box className="About-Title">タイトル</Box>
+    
+      <Box className="About-Title"><span className="Title-hed">A</span>bout</Box>
+      <Box className="About-underTitle">私たちについて</Box>
+<img className="Aboutimg" src={Aboutimg} alt="" />
 <Box className="About-content">
       副業人材を主体とした「業務委託サービス」です。 <br/>
   通常では人材派遣の市場に出てこないような大企業に勤めているベテランのエンジニアや開発の中核を担う  <br/>
@@ -16,7 +19,6 @@ export const About = () => {
 </Box>
 
 
-
     </Box>
   );
 };
@@ -26,16 +28,28 @@ const sx: SxProps<Theme> = {
     width: "100%",
     padding:"0",
     display: "inline-block",
+    backgroundColor:"rgba(34, 161, 185, 0.1)",
   },
   ".About-Title":{
-    marginTop: "50px",
+    fontSize:"36px",
+    fontWeight:700,
+    textAlign:"center"
   },
   ".About-content":{
-    textAlign:"center"
-  }
-};
-
-"About-size":{
-display:"flex",
-flexDirection:"row-reverse",
+    textAlign:"center",
+    fontSize:"18px",
+    Weight:"400"
+  },
+  ".Title-hed":{
+    color:"#22A1B9",
+    fontSize:"36px",
+  },
+  ".About-underTitle":{
+    textAlign:"center",
+    fontSize:"16px",
+    Weight:700,
+  },
+".Aboutimg":{
+width:"100%"
 }
+};
